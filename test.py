990c7001f884a4412a9b5354c2bc9fc05298ed87
JamesMun <rkwo112@gmail.com>
2018-01-23 10:17:48 +0900

    First commit
    A function that tests '**kwargs'.

---
+++ test.py
@@ -0,0 +1,19 @@
+def my_func(**kwargs):
+    """A function that shows how powerful **kwargs is"""
+    first_arg = kwargs.get('first_arg', -1)
+
+    if isinstance(first_arg, list):
+        print "first_arg is a list"
+
+    print "First_arg: ", kwargs.get('first_arg',-1)
+    print "second_arg: ", kwargs.get('second_arg',-2)
+    print "third_arg: ", kwargs.get('third_arg',-3)
+    print "fourth_arg: ", kwargs.get('fourth_arg',-4)
+
+    for i in xrange(101, 0, -3):
+        print i
+
+    pass
+
+my_func(first_arg=['a','b','c'],
+        second_arg={'x': 3, 'y': 2,'z': 3})
